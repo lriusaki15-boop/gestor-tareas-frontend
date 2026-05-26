08ee5fc9a3d0c38d272aa197cdc225e70dccfaaa
anaya <anaya@LAPTOP-9FAIHIOC>
2026-05-26 20:08:13 +0200

    Añadido pantalla login y de registrar con estillos basicos pero sin funcionalidad por ahora y hay que apañar los estilos para que sean resposib y funcionales los nuevos apartados

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TareaCard } from './components/tarea-card/tarea-card';
 import { Login } from "./components/login/login";
+import { Registrar } from "./components/registrar/registrar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TareaCard, Login],
+  imports: [RouterOutlet, TareaCard, Login, Registrar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/registrar/registrar.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Registrar } from './registrar';
+
+describe('Registrar', () => {
+  let component: Registrar;
+  let fixture: ComponentFixture<Registrar>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Registrar],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Registrar);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
