0cad445ce21923a7374308f7d16583762dbb2932
Miguel Cervera <l.riusaki15@gmail.com>
2026-05-27 13:52:34 +0200

    Cambios para que funcione el registras pero no funciona

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Registrar } from './components/registrar/registrar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Login },
+  { path: 'registro', component: Registrar }
+];
