2a157f5b42e201ff4fdf94474d3753bf0239b6a2
anaya <anaya@LAPTOP-9FAIHIOC>
2026-05-28 19:55:17 +0200

    Creando la pantalla principal, hay que iniciar el adecuar la pantalla para que funcione y muestre las tareas y que si es admin pueda acceder a unas pantallas que estaran ocultas para el cliente y el admin tendra acceso a otras pantallas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Registrar } from './components/registrar/registrar';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
   { path: '', component: Login },
-  { path: 'registrar', component: Registrar }
+  { path: 'registrar', component: Registrar },
+  { path: 'home', component: Home }
 ];
